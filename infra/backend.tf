d9dc4f3f2fa1315c843e96127ad7fdfbc1cfd2df
Desalew <dessalew186@gmail.com>
2026-05-17 07:48:46 -0500

    fix: correct terraform file contents

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,14 +1,2 @@
-$content = @'
-# Backend disabled - using local state for now
-# Uncomment below once Azure storage account is created
-#
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "tfstate-rg"
-#     storage_account_name = "tfstateapim001"
-#     container_name       = "tfstate"
-#     key                  = "apim.terraform.tfstate"
-#   }
-# }
-'@
-[System.IO.File]::WriteAllText("$PWD\infra\backend.tf", $content, [System.Text.Encoding]::ASCII)
+$backend = "# local state - no remote backend"
+[System.IO.File]::WriteAllText("$PWD\infra\backend.tf", $backend, [System.Text.Encoding]::ASCII)
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,14 +1,2 @@
-$content = @'
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "apim_gateway_url" {
-  value = azurerm_api_management.apim.gateway_url
-}
-
-output "apim_portal_url" {
-  value = azurerm_api_management.apim.developer_portal_url
-}
-'@
-[System.IO.File]::WriteAllText("$PWD\infra\outputs.tf", $content, [System.Text.Encoding]::ASCII)
+$outputs = "output `"resource_group_name`" {`n  value = azurerm_resource_group.rg.name`n}`noutput `"apim_gateway_url`" {`n  value = azurerm_api_management.apim.gateway_url`n}"
+[System.IO.File]::WriteAllText("$PWD\infra\outputs.tf", $outputs, [System.Text.Encoding]::ASCII)
